873ba033c633975a8b9ab335a7f81cea18eb3e1e
MJ <martinjirku@gmail.com>
2016-04-30 17:46:06 +0200

    setup zsh

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,71 +9,52 @@ else
   git="/usr/bin/git"
 fi
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+white() {
+  echo "%{$fg[white]%}"
 }
 
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
+red() {
+  echo "%{$fg[red]%}"
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+green() {
+  echo "%{$fg[green]%}"
 }
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
+cyan() {
+  echo "%{$fg[cyan]%}"
 }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
+yellow() {
+  echo "%{$fg[yellow]%}"
 }
 
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
+reset_color() {
+  echo "%{$reset_color%}"
 }
 
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
+get_time_date() {
+  echo "$(white)[$(red)%* %D$(reset_color)$(white)]"
 }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+get_user_and_machine() {
+  echo "$(green) %B%n%b$(white)@%M$(reset_color)"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+get_path() {
+  echo "$(cyan)%B%d%b$(yellow)"
+}
+
+source "${ZSH}/zsh/zsh-git-prompt/zshrc.sh"
+
+#overwrite ZSH_THEME_GIT_PROMPT_PREFIX variable
+ZSH_THEME_GIT_PROMPT_PREFIX=" ("
+
+export PROMPT='
+$(get_time_date)$(get_user_and_machine)$(git_super_status)$(white):
+$(get_path) $ $(reset_color)'
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+  export RPROMPT="%{$fg_bold[cyan]%}$(reset_color)"
 }
 
 precmd() {
