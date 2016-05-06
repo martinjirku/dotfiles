fde1bc588dbb7f3eca0770c5bb6b3d2f08be91d6
MJ <martinjirku@gmail.com>
2016-05-06 20:29:06 +0200

    add installable atom

---
+++ atom.symlink/init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+#
+apm install Parinfer
+apm install atom-handlebars
+apm install atom-ternjs
+apm install auto-detect-indentation
+apm install docblockr
+apm install docs-snippets
+apm install highlight-selected
+apm install ink
+apm install jsdoc-generator
+apm install lisp-paredit
+apm install proto-repl
+apm install proto-repl-charts
+apm install set-syntax
+apm install turbo-javascript
+apm install vim-mode-plus
