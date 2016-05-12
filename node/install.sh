6db6969e655f806152b9fff6759e7a0f0eed4504
MJ <martinjirku@gmail.com>
2016-05-12 17:46:33 +0200

    spacemacs

--- node/install.sh
+++ node/install.sh
@@ -2,3 +2,8 @@ if test ! $(which spoof)
 then
   sudo npm install spoof -g
 fi
+
+if test ! $(which gulp)
+then
+  sudo npm i gulp -g
+fi
--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,3 +1,3 @@
 echo "[spacemacs] installing js the dependencies"
+npm i -g eslint-config-airbnb bower tern js-beautify jshint eslint babel-eslint eslint-plugin-react eslint-plugin-react-native
 bower install git://github.com/adobe-fonts/source-code-pro.git#release
-npm i -g tern js-beautify jshint eslint babel-eslint eslint-plugin-react
