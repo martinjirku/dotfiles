84468d164fb617f1fcc9c136006bb7d12689167d
MJ <martinjirku@gmail.com>
2016-05-12 22:36:57 +0200

    installationi file of haskell

--- haskell/install.sh
+++ haskell/install.sh
@@ -1,3 +1,28 @@
-if ! [ -x `which cabal` ]; then
-				cabal install cabal-install
-fi
+info () {
+	printf "\r ℹ️  $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;33m✔︎ OK\033[0m ] $1\n"
+}
+
+
+install () {
+	local name=$1
+	if ! type $name &> /dev/null; then
+			tput sc #save cursor
+			info "$name installing..."
+			cabal install $name > /dev/null
+			tput rc;tput el; # rc = restore cursor, el = erase to end of line
+			success "$name installed"
+		else
+			tput rc;tput el;
+			success "$name already installed"
+	fi
+}
+
+install "cabal"
+install "stylish-haskell"
+install "hlint"
+install "hasktags"
+install "ghc-mod"
