13e13593d0441d3f232c566d18d668212c1e462b
MJ <martinjirku@gmail.com>
2016-05-13 22:00:06 +0200

    df

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -1,14 +1,27 @@
-i () {
-    local package=$1
-    if [[ ! -d "$HOME/.atom/packages/$package" ]]
-    then
-        apm install $package --verbose
-    else
-        printf "package already installed: \t $package \n"
-    fi
+info () {
+	echo "\r ℹ️  $1"
 }
 
-#
+success () {
+	echo "\r\033[2K  [ \033[00;33m✔︎ OK\033[0m ] $1"
+}
+
+i () {
+	local package=$1
+	if [[ ! -d "$HOME/.atom/packages/$package" ]]
+	then
+		tput sc #save cursor
+		info "$package installing..."
+		apm install $package
+		tput rc;tput el; # rc = restore cursor, el = erase to end of line
+		success "$package installed"
+	else
+		success "$package package already installed"
+	fi
+}
+
+
+#installation
 i Parinfer
 i atom-handlebars
 i atom-ternjs
@@ -23,4 +36,6 @@ i proto-repl
 i proto-repl-charts
 i set-syntax
 i turbo-javascript
-i vim-mode-plus
+#i vim-mode-plus
+i ide-haskell
+i haskell-grammar
