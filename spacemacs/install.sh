d5aff17faca4dab2907b364eab10e4aa8add1e48
Martin MJ Jirku <MJirku2@sk.ibm.com>
2017-10-13 17:14:51 +0200

    adds vim configuration - basics

--- spacemacs/install.sh
+++
@@ -1,3 +0,0 @@
-echo "[spacemacs] installing js the dependencies"
-npm i -g eslint-config-airbnb bower tern js-beautify jshint eslint babel-eslint eslint-plugin-react eslint-plugin-react-native
-bower install git://github.com/adobe-fonts/source-code-pro.git#release
