f5fc0e3f18369a529032f6d9b4b408fc73de7233
Martin MJ Jirku <MJirku2@sk.ibm.com>
2017-10-15 10:06:59 +0200

    add vim elixir support

--- vim.symlink/install.sh
+++ vim.symlink/install.sh
@@ -1,2 +1,20 @@
-curl -fLo ./autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#!/bin/sh
+
+
+
+mkdir -p  ~/dotfiles/vim.symlink/autoload
+mkdir -p ~/dotfiles/vim.symlink/compiler
+mkdir -p ~/dotfiles/vim.symlink/ftdetect
+mkdir -p ~/dotfiles/vim.symlink/ftplugin
+mkdir -p ~/dotfiles/vim.symlink/indent
+mkdir -p ~/dotfiles/vim.symlink/syntax
+
+# elixir plugin
+echo installing elixir plugin from $ELIXIR_PLUGIN_PATH
+
+cp -R ~/dotfiles/vim.symlink/bundle/vim-elixir/autoload/ ~/dotfiles/vim.symlink/autoload
+cp -R ~/dotfiles/vim.symlink/bundle/vim-elixir/compiler/ ~/dotfiles/vim.symlink/compiler
+cp -R ~/dotfiles/vim.symlink/bundle/vim-elixir/ftdetect/ ~/dotfiles/vim.symlink/ftdetect
+cp -R ~/dotfiles/vim.symlink/bundle/vim-elixir/ftplugin/ ~/dotfiles/vim.symlink/ftplugin
+cp -R ~/dotfiles/vim.symlink/bundle/vim-elixir/indent/ ~/dotfiles/vim.symlink/indent
+cp -R ~/dotfiles/vim.symlink/bundle/vim-elixir/syntax/ ~/dotfiles/vim.symlink/syntax
