acf59a50aa9d6d6e3e5b77809259245bf3475cfe
Martin Jirku <martin@jirku.sk>
2025-07-11 20:24:37 +0200

    add vim mode to zsh
    Signed-off-by: Martin Jirku <martin@jirku.sk>

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -4,3 +4,29 @@ setopt EXTENDED_GLOB
 setopt PROMPT_SUBST
 setopt LIST_PACKED
 setopt LIST_ROWS_FIRST
+
+
+# vi mode
+set -o vi
+
+# Reduce key delay (default is 0.4s which feels sluggish)
+export KEYTIMEOUT=1
+
+# todo: extract somewhere else
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'  # block cursor
+  elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'  # beam cursor
+  fi
+}
+zle -N zle-keymap-select
+
+# Copy to system clipboard
+bindkey -M vicmd 'y' vi-yank-system-clipboard
+bindkey -M vicmd 'Y' vi-yank-eol-system-clipboard
+
+# Paste from system clipboard
+bindkey -M vicmd 'p' vi-put-system-clipboard-after
+bindkey -M vicmd 'P' vi-put-system-clipboard-before
+
